d915472095588df0e68c4af6671199e829a13358
acpcarvalhoh <104265224+acpcarvalhoh@users.noreply.github.com>
2022-06-15 14:53:22 -0300

    Create ex17.js

---
+++ aula 12/ex17.js
@@ -0,0 +1,31 @@
+var dia = new Date()
+var hoje =  dia.getDay()
+
+
+switch(hoje){
+    case 0:
+    console.log("DOMIGO")
+    break
+    case 1:
+    console.log("SEGUNDA")
+    break
+    case 2:
+    console.log("TERÇA")
+    break
+    case 3:
+    console.log("QUARTA")
+    break
+    case 4:
+    console.log("QUINTA")
+    break
+    case 5:
+    console.log("SEXTA")
+    break
+    case 6:
+    console.log("SABADO")
+    break
+}
+
+
+console.log(hoje)
+
