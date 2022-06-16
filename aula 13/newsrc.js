966153088315c27bdeb4b2579da1f337496d60a8
acpcarvalhoh <104265224+acpcarvalhoh@users.noreply.github.com>
2022-06-16 00:34:52 -0300

    Update newsrc.js

--- aula 13/newsrc.js
+++ aula 13/newsrc.js
@@ -3,9 +3,8 @@ function carregar (){
     var msg = document.querySelector('#msg')
     var img = document.querySelector('#imagem')
     var data = new Date()
-    var horas = data.getHours()
-    var min = data.getMinutes()
-    msg.innerHTML = `Agora são ${horas}:${min}`
+    var horas = data.toLocaleTimeString()
+    msg.innerHTML = `Agora são ${horas}`
     if (horas > 0 && horas <= 12){
         img.src = '../aula 13/imagens/manha.png'
         window.document.body.style.backgroundColor = '#fff'
@@ -15,11 +14,12 @@ function carregar (){
         
         window.document.body.style.backgroundColor = '#c46e00'
 
-    } else{
+    } else {
         img.src = '../aula 13/imagens/noite.png'
         window.document.body.style.backgroundColor = '#0000008a'
         msg.innerHTML += `<P>BOA NOITE!!</P>`
         
+    
     }
 
     
