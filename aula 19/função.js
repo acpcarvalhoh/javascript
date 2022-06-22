daa8ac6398326a66750e32e2ad79af1286b32177
acpcarvalhoh <104265224+acpcarvalhoh@users.noreply.github.com>
2022-06-22 14:15:29 -0300

    update js

---
+++ aula 19/função.js
@@ -0,0 +1,14 @@
+function ParImpar(n) {
+    if (n % 2 == 0){
+        return 'PAR'
+    }else{
+        return 'Impar'
+    }
+
+
+}
+
+
+let res = ParImpar(4)
+
+console.log(res)
---
+++ aula 19/função03.js
@@ -0,0 +1,8 @@
+var v = function soma(x){
+    return x*2
+
+}
+
+
+
+console.log(v(10))
